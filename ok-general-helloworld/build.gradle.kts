0beb06499fff409427bee8c69b7db5f2ac1babcc
Sergey Okatov <sokatov@gmail.com>
2020-12-23 11:15:09 +0500

    M1L1-s3-hello world добавлен .gitignore

--- ok-general-helloworld/build.gradle.kts
+++ ok-general-helloworld/build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     kotlin("jvm") version "1.4.21"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "ru.otus.otuskotlin.general"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -11,4 +11,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
 }
---
+++ ok-general-helloworld/src/test/kotlin/ru/otus/otuskotlin/general/hello/world/HelloWorldKtTest.kt
@@ -0,0 +1,11 @@
+package ru.otus.otuskotlin.general.hello.world
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class HelloWorldKtTest {
+    @Test
+    fun firstTest() {
+        assertEquals(1, 2-1)
+    }
+}
