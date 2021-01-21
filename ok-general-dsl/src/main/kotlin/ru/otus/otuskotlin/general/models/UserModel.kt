fbd043a0b34dbd9d2fb0124206c2902d6d27d623
Sergey Okatov <sokatov@gmail.com>
2021-01-21 11:09:48 +0500

    M1L4 s3 Разработка целевой модели, которую будет строить DSL.Fix dob

--- ok-general-dsl/src/main/kotlin/ru/otus/otuskotlin/general/models/UserModel.kt
+++ ok-general-dsl/src/main/kotlin/ru/otus/otuskotlin/general/models/UserModel.kt
@@ -1,13 +1,13 @@
 package ru.otus.otuskotlin.general.models
 
-import java.time.Instant
+import java.time.LocalDate
 
 data class UserModel(
     var id: UserId = UserId.NONE,
     var fname: String = "",
     var mname: String = "",
     var lname: String = "",
-    var dob: Instant = Instant.MIN,
+    var dob: LocalDate = LocalDate.MIN,
     var email: Email = Email.NONE,
     var phone: Phone = Phone.NONE,
     var permissions: MutableSet<UserPermissions> = mutableSetOf()
