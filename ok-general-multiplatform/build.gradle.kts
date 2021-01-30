f2918a3f8e304d2c1ee55993e8d4a8c7847ad2af
Sergey Okatov <sokatov@gmail.com>
2021-01-30 10:13:03 +0500

    M1L7 s1 Настройка Gradle для работы Kotlin Multiplatform

---
+++ ok-general-multiplatform/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    kotlin("multiplatform") version "1.4.21"
+}
+
+group = rootProject.group
+version = rootProject.version
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+kotlin {
+    /* Targets configuration omitted. 
+    *  To find out how to configure the targets, please follow the link:
+    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
+    js {
+        browser {  }
+        nodejs {  }
+    }
+    jvm {
+        withJava()
+    }
+    linuxX64 {  }
+
+    sourceSets {
+        val coroutinesVersion = "1.4.2"
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        val linuxX64Main by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+            }
+        }
+        val linuxX64Test by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ rootProject.name = "202012-otuskotlin-general"
 include("ok-corotuines-practice")
 include("ok-general-helloworld")
 include("ok-general-dsl")
+include("ok-general-multiplatform")
