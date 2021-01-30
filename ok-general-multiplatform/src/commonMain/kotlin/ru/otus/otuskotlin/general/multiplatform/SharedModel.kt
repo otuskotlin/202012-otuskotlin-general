621f07c12db05f13ea94fe0f0bbf9ffb83160c4e
Sergey Okatov <sokatov@gmail.com>
2021-01-30 11:29:02 +0500

    M1L7 s3 Интероперабельность с Java

--- ok-general-multiplatform/src/commonMain/kotlin/ru/otus/otuskotlin/general/multiplatform/SharedModel.kt
+++ ok-general-multiplatform/src/commonMain/kotlin/ru/otus/otuskotlin/general/multiplatform/SharedModel.kt
@@ -1,7 +1,14 @@
 package ru.otus.otuskotlin.general.multiplatform
 
-data class SharedModel(
+import kotlin.js.JsName
+import kotlin.jvm.JvmName
+import kotlin.jvm.JvmOverloads
+
+data class SharedModel @JvmOverloads constructor(
     val id: String = "",
     val name: String = ""
 ) {
+    @JsName("my_name")
+    @get:JvmName("getMyName")
+    val `my name` = "my name"
 }
--- ok-general-multiplatform/src/jvmMain/kotlin/ru/otus/otuskotlin/general/multiplatform/SharedClass.kt
+++ ok-general-multiplatform/src/jvmMain/kotlin/ru/otus/otuskotlin/general/multiplatform/SharedClass.kt
@@ -3,6 +3,8 @@ package ru.otus.otuskotlin.general.multiplatform
 import kotlinx.coroutines.delay
 
 actual class SharedClass {
+    val `my name`: String = "my name"
+
     actual suspend fun request(id: String): String {
         delay(1000)
         return "Jvm done"
